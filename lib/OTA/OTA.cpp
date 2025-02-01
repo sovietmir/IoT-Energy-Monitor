9f3059bc5815f19175eceda48390f468a850315c
sovietmir <s97130@hotmail.com>
2025-02-01 23:15:09 +0200

    OTA review and mainaining consistency

--- lib/OTA/OTA.cpp
+++ lib/OTA/OTA.cpp
@@ -1,10 +1,11 @@
 #include "OTA.h"
 
-OTA::OTA(HTTPServerManager& serverManager, Logger& logger) : _serverManager(serverManager),_logger(logger) {}
+OTA::OTA(HTTPServerManager& serverManager, Logger* logger) : _serverManager(serverManager),_logger(logger) {}
 
 void OTA::begin(){
     registerEndpoints();
 }
+
 void OTA::registerEndpoints() {
     // Register firmware upload endpoint
     _serverManager.registerPage(
@@ -31,15 +32,15 @@ void OTA::registerEndpoints() {
     );
     
     _serverManager.registerPage("/api/reboot", HTTP_GET, [this](ESP8266WebServer& server) {
-        _logger.log("URI: /api/");
-        server.send(200, "application/json", "{\"message\":\"The microcontroller shall reboot in half a second...\"}");
+        if (_logger != nullptr) _logger->log("URI: /api/");
+        server.send(200, "application/json", "{\"status\": 1, \"message\": \"Microcontroller shall reboot in half a second.\"}"); 
         delay(500);
         ESP.restart();
     });
 
     // Register directory listing
     _serverManager.registerPage("/api/directories", HTTP_GET, [this](ESP8266WebServer& server) {
-        _logger.log("URI: /api/directories");
+        if (_logger != nullptr) _logger->log("URI: /api/directories");
         handleDirectoryList(server);
     });
 
@@ -62,6 +63,21 @@ void OTA::registerEndpoints() {
     });
 }
 
+void OTA::addReportStepHook(std::function<void(int)> func) {
+  _reportStepsHooks.push_back(func);
+}
+void OTA::reportStep(int step){
+  for (auto& hook : _reportStepsHooks) {
+    if (hook) {
+        hook(step);
+    }
+  }
+}
+
+/**
+ * @brief Handles firmware upload via HTTP POST request.
+ * @param server Reference to the web server instance managing the request.
+ */
 void OTA::handleFirmwareUpload(ESP8266WebServer& server) {
     HTTPUpload& upload = server.upload();
     uint32_t update_size = ((ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000);
@@ -69,13 +85,13 @@ void OTA::handleFirmwareUpload(ESP8266WebServer& server) {
     reportStep(1);
     //Update.runAsync(true);
     if (upload.status == UPLOAD_FILE_START) {
-        _logger.log("Start firmware update: "+upload.filename+".\n");
+        if (_logger != nullptr) _logger->log("Start firmware update: "+upload.filename+".\n");
        if (!Update.begin(update_size)) { // Begin OTA process
             reportStep(2);
             reportStep(-3);
             Update.printError(Serial);
-            server.send(500, "application/json", "{\"error\":\"Firmware update failed to start.\"}");
-            _logger.log("Firmware update failed to start.\n");
+            server.send(500, "application/json", "{\"status\": -1, \"error\":\"Firmware update failed to start.\"}");
+            if (_logger != nullptr) _logger->log("Firmware update failed to start.\n");
             return;
         }
         reportStep(2);
@@ -86,25 +102,29 @@ void OTA::handleFirmwareUpload(ESP8266WebServer& server) {
         reportStep(2);
     } else if (upload.status == UPLOAD_FILE_END) {
         if (Update.end(true)) { // End OTA process
-            server.send(200, "application/json", "{\"message\":\"Firmware updated successfully. Rebooting...\"}");
-            _logger.log("Firmware update successful. Rebooting...\n");
+            server.send(200, "application/json", "{\"status\": -1, \"message\":\"Firmware updated successfully. Rebooting...\"}");
+            if (_logger != nullptr) _logger->log("Firmware update successful. Rebooting...\n");
             delay(500);
             ESP.restart();
         } else {
             reportStep(-3);
             Update.printError(Serial);
-            server.send(500, "application/json", "{\"error\":\"Firmware update failed.\"}");
-            _logger.log("Firmware update failed.\\n");
+            server.send(500, "application/json", "{\"status\": -2, \"error\":\"Firmware update failed.\"}");
+            if (_logger != nullptr) _logger->log("Firmware update failed.\\n");
         }
     } else if (upload.status == UPLOAD_FILE_ABORTED) {
         reportStep(2);
         reportStep(-3);
         Update.end();
-        server.send(500, "application/json", "{\"error\":\"Firmware update aborted.\"}");
-        _logger.log("Firmware update aborted.\\n");
+        server.send(500, "application/json", "{\"status\": -3, \"error\":\"Firmware update aborted.\"}");
+        if (_logger != nullptr) _logger->log("Firmware update aborted.\\n");
     }
 }
 
+/**
+ * @brief Handles file upload via HTTP POST request.
+ * @param server Reference to the web server instance managing the request.
+ */
 void OTA::handleFileUpload(ESP8266WebServer& server) {
     HTTPUpload& upload = server.upload();
     static File file;         // Keeps file open across status changes
@@ -112,23 +132,23 @@ void OTA::handleFileUpload(ESP8266WebServer& server) {
     
      reportStep(1);
     if (upload.status == UPLOAD_FILE_START) {
-        _logger.log("Start file upload: "+upload.filename+";");
+        if (_logger != nullptr) _logger->log("Start file upload: "+upload.filename+";");
         directory = server.arg("directory"); // Get the directory from the form
         if (directory.isEmpty()) directory = "/"; // Default to root if not provided
 
         String path = directory  + "/" + upload.filename; // Add subdirectory here
         file = LittleFS.open(path, "w");
         if (!file) {
-            server.send(500, "application/json", "{\"error\":\"Failed to open file for writing.\"}");
-            _logger.log(" Failed to open file '"+path+"' for writing.\n");
+            server.send(500, "application/json", "{\"status\": -1, \"error\":\"Failed to open file for writing.\"}");
+            if (_logger != nullptr) _logger->log(" Failed to open file '"+path+"' for writing.\n");
             reportStep(2);
             reportStep(-3);
             return;
         }
-        _logger.log("\n");
+        if (_logger != nullptr) _logger->log("\n");
         reportStep(2);
     } else if (upload.status == UPLOAD_FILE_WRITE) {
-        _logger.log("File uploading (writing)...\n");        
+        if (_logger != nullptr) _logger->log("File uploading (writing)...\n");        
         if (file) {
             file.write(upload.buf, upload.currentSize);
         }
@@ -136,11 +156,11 @@ void OTA::handleFileUpload(ESP8266WebServer& server) {
     } else if (upload.status == UPLOAD_FILE_END) {
         if (file) {
             file.close();            
-            server.send(200, "application/json", "{\"message\":\"File uploaded successfully.\"}");
-            _logger.log("File upload successful.\n");
+            server.send(200, "application/json", "{\"status\": 1, \"message\":\"File uploaded successfully.\"}");
+            if (_logger != nullptr) _logger->log("File upload successful.\n");
         } else {
-            server.send(500, "application/json", "{\"error\":\"Failed to save file.\"}");
-            _logger.log("Failed to save file.\n");
+            server.send(500, "application/json", "{\"status\": -2, \"error\":\"Failed to save file.\"}");
+            if (_logger != nullptr) _logger->log("Failed to save file.\n");
             reportStep(-2);
         }
         reportStep(2);
@@ -149,13 +169,17 @@ void OTA::handleFileUpload(ESP8266WebServer& server) {
             file.close();
             LittleFS.remove(upload.filename); // Clean up
         }
-        server.send(500, "application/json", "{\"error\":\"File upload aborted.\"}");
-        _logger.log("File upload aborted.\n");
+        server.send(500, "application/json", "{\"status\": -3, \"error\":\"File upload aborted.\"}");
+        if (_logger != nullptr) _logger->log("File upload aborted.\n");
         reportStep(2);
         reportStep(-1);
     }
 }
 
+/**
+ * @brief Handles directory listing via HTTP GET request.
+ * @param server Reference to the web server instance managing the request.
+ */
 void OTA::handleDirectoryList(ESP8266WebServer& server) {
     String response = "[";
     Dir dir = LittleFS.openDir("/");
@@ -169,13 +193,11 @@ void OTA::handleDirectoryList(ESP8266WebServer& server) {
 
 
 /**
- * Handles listing files.
- *
- * @param server Reference to the ESP8266WebServer instance managing the request.
- *               Used to access request parameters and send responses.
+ * @brief Handles file system requests via HTTP GET request.
+ * @param server Reference to the web server instance managing the request.
  */
 void OTA::handleFileSystemRequest(ESP8266WebServer& server) {
-    _logger.log("handleFileSystemRequest: ");
+    if (_logger != nullptr) _logger->log("handleFileSystemRequest: ");
     JsonDocument  doc;                     // Create a JSON document to store file data.
     JsonArray files = doc["files"].to<JsonArray>();
 
@@ -192,16 +214,93 @@ void OTA::handleFileSystemRequest(ESP8266WebServer& server) {
     String response;
     serializeJson(doc, response);
     server.send(200, "application/json", response);
-    _logger.log("ok\n");    
+    if (_logger != nullptr) _logger->log("ok\n");    
 }
 
+/**
+ * @brief Handles file download requests.
+ * @param server Reference to the web server instance managing the request.
+ */
+void OTA::handleDownloadRequest(ESP8266WebServer& server) {
+    String filePath = server.arg("file"); // Extract the 'file' parameter from the request if provided.
+    if (_logger != nullptr) _logger->log("handleDownloadRequest '"+filePath+"'");
+
+    if (LittleFS.exists(filePath)) {
+        File file = LittleFS.open(filePath, "r"); // Attempt to open the requested file in read mode.
+        server.streamFile(file, "application/octet-stream");
+        file.close();
+    } else {
+        server.send(404, "application/json", "{\"status\": -1, \"error\":\"File not found\"}");
+    }
+    if (_logger != nullptr) _logger->log("\n");
+   
+}
+
+/**
+ * @brief Handles file deletion requests.
+ * @param server Reference to the web server instance managing the request.
+ */
+void OTA::handleDeleteRequest(ESP8266WebServer& server) {
+    String path = server.arg("path");
+    if (_logger != nullptr) _logger->log("handleDeleteRequest: "+path);
+    if (LittleFS.exists(path)) { // Check if the file exists.
+        if (LittleFS.remove(path)) { // Delete the file.
+            server.send(200, "application/json", "{\"status\": 1, \"message\":\"File deleted successfully\"}");
+        } else {
+            server.send(500, "application/json", "{\"status\": -1, \"error\":\"Failed to delete file\"}");
+        }            
+    } else {
+        server.send(404, "application/json", "{\"status\": -2, \"error\":\"File not found\"}");
+    }
+    if (_logger != nullptr) _logger->log("\n");
+   
+}
+
+
+/**
+ * Handles requests to create a new directory. The request to create a direcotry 
+ * is POST, with plain payload that contains an object which is JSON encoded. 
+ * The object should have fields 'parentPath' and 'dirName'.
+ * 
+ * @brief Handles requests to create a new directory.
+ * @param server Reference to the web server instance managing the request.
+ */
+void OTA::handleAddDirectoryRequest(ESP8266WebServer& server) {
+    if (!server.hasArg("plain")) {
+        server.send(400, "application/json", "{\"status\": -1, \"error\":\"Bad Request\"}");
+        return;
+    }
+    
+    String json = server.arg("plain");
+    JsonDocument params;
+    DeserializationError error = deserializeJson(params, json);
+    if(error) {
+        server.send(500, "application/json", "{\"status\": -2, \"error\":\"Failed to deserializeJson the request data\"}");
+        return;
+    }
+
+    String parentPath = params["parentPath"];//server.arg("parentPath");
+    String dirName =  params["dirName"];// server.arg("dirName");
+    String fullPath = parentPath + "/" + dirName;
+    if (_logger != nullptr) _logger->log("handleAddDirectoryRequest '"+fullPath+"': ");
+
+    if (LittleFS.mkdir(fullPath)) {
+        server.send(200, "application/json", "{\"status\": 1, \"message\":\"Directory created successfully\"}");
+        if (_logger != nullptr) _logger->log("ok\n");
+    } else {
+        server.send(500, "application/json", "{\"status\": -3, \"error\":\"Failed to create directory\"}");
+        if (_logger != nullptr) _logger->log("nok\n");
+    }
+   
+}
 
 /**
  * Recursively retrieves all files and directories from the file system,
  * starting from the specified directory.
  *
- * @param dirPath The path to the directory to start the traversal.
- * @param filesArray A JSON array to store file and directory details.
+ * @brief Recursively lists files and directories in a given path.
+ * @param dirPath  The path to start listing from.
+ * @param filesArray JSON array to store file and directory details.
  */
 void OTA::listFilesRecursive(const String& dirPath, JsonArray& filesArray) {
     Dir dir = LittleFS.openDir(dirPath); // Open the directory at dirPath.
@@ -218,102 +317,3 @@ void OTA::listFilesRecursive(const String& dirPath, JsonArray& filesArray) {
     }
 }
 
-
-
-
-/**
- * Handles  downloading a file
- * 
- * @param server Reference to the ESP8266WebServer instance managing the request.
- *               Used to access request parameters and send responses.
- */
-void OTA::handleDownloadRequest(ESP8266WebServer& server) {
-    String filePath = server.arg("file"); // Extract the 'file' parameter from the request if provided.
-    _logger.log("handleDownloadRequest '"+filePath+"'");
-
-    if (LittleFS.exists(filePath)) {
-        File file = LittleFS.open(filePath, "r"); // Attempt to open the requested file in read mode.
-        server.streamFile(file, "application/octet-stream");
-        file.close();
-    } else {
-        server.send(404, "application/json", "{\"error\":\"File not found\"}");
-    }
-    _logger.log("\n");
-   
-}
-
-/**
- * Handles delete file
- * 
- * @param server Reference to the ESP8266WebServer instance managing the request.
- *               Used to access request parameters and send responses.
- */
-void OTA::handleDeleteRequest(ESP8266WebServer& server) {
-    String path = server.arg("path");
-    _logger.log("handleDeleteRequest: "+path);
-    if (LittleFS.exists(path)) { // Check if the file exists.
-        if (LittleFS.remove(path)) { // Delete the file.
-            server.send(200, "application/json", "{\"status\":\"File deleted successfully\"}");
-        } else {
-            server.send(500, "application/json", "{\"error\":\"Failed to delete file\"}");
-        }            
-    } else {
-        server.send(404, "application/json", "{\"error\":\"File not found\"}");
-    }
-    _logger.log("\n");
-   
-}
-
-
-/**
- * Handles add director, i.e., creates given direcotry
- * The request to create a direcotry is POST, with plain paylod that contains an object 
- * which is JSON encoded. The object should have fields 'parentPath' and 'dirName'
- * 
- * @param server Reference to the ESP8266WebServer instance managing the request.
- *               Used to access request parameters and send responses.
- */
-void OTA::handleAddDirectoryRequest(ESP8266WebServer& server) {
-    if (!server.hasArg("plain")) {
-        server.send(400, "application/json", "{\"error\":\"Bad Request\"}");
-        return;
-    }
-    
-    String json = server.arg("plain");
-    JsonDocument params;
-    DeserializationError error = deserializeJson(params, json);
-    if(error) {
-        server.send(500, "application/json", "{\"error\":\"Failed to deserializeJson the request data\"}");
-        return;
-    }
-
-    String parentPath = params["parentPath"];//server.arg("parentPath");
-    String dirName =  params["dirName"];// server.arg("dirName");
-    String fullPath = parentPath + "/" + dirName;
-    _logger.log("handleAddDirectoryRequest '"+fullPath+"': ");
-
-    if (LittleFS.mkdir(fullPath)) {
-        server.send(200, "application/json", "{\"status\":\"Directory created successfully\"}");
-        _logger.log("ok\n");
-    } else {
-        server.send(500, "application/json", "{\"error\":\"Failed to create directory\"}");
-        _logger.log("nok\n");
-    }
-   
-}
-
-
-
-
-
-
-void OTA::addReportStepHook(std::function<void(int)> func) {
-  reportStepsHooks.push_back(func);
-}
-void OTA::reportStep(int step){
-  for (auto& hook : reportStepsHooks) {
-    if (hook) {
-        hook(step);
-    }
-  }
-}
